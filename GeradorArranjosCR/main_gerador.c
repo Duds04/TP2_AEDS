7b85cd14b86c2f770fd056aa10faf314fec11d4d
João Bradachi <105111795+JBradachi@users.noreply.github.com>
2022-11-01 17:09:41 -0300

    Update main_gerador.c

--- GeradorArranjosCR/main_gerador.c
+++ GeradorArranjosCR/main_gerador.c
@@ -1,23 +1,8 @@
 #include "Functions.h"
 
-// O programa em C irá printar todas as possibilidades de arranjos com repetição
-
-int main()
+int main(int argc, char *argv[])
 {
-    int r, n, i;
+	Arranjos(3);
 
-    printf("Digite quantas cores estao disponiveis\n>>> ");
-    scanf("%d", &n);
-
-    printf("Digite o numero de espacos na arvore\n>>> ");
-    scanf("%d", &r);
-    printf("\n");
-
-	int arr[n];
-    for(i=0; i<n;i++){
-        arr[i] = i;
-    }
-	
-	CombinationRepetition(arr, n, r);
 	return 0;
 }
