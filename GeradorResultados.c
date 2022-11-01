6c79f46e2320a61f5a3c047f2a5261e564fa5129
João Bradachi <105111795+JBradachi@users.noreply.github.com>
2022-11-01 17:10:32 -0300

    Update GeradorResultados.c

--- GeradorResultados.c
+++ GeradorResultados.c
@@ -1,37 +1,50 @@
-#include "./headers/GeradorResultados.h"
+#include "Functions.h"
 
+char *vetor[] = {"0",  "1", NULL} ;
 
-void CombinationRepetitionUtil(int chosen[], int arr[], int index, int r, int start, int end)
-{
-	// Como o índice se tornou r, a combinação atual está pronta para ser impressa
-
-	if (index == r)
-	{
-		for (int i = 0; i < r; i++)
-			printf("%d ", arr[chosen[i]]);
-		printf("\n");
-		return;
-	}
-
-	// Um por um, é escolhido todos os elementos (sem considerar o fato de o elemento 
-    // já estar escolhido ou não), e se repete até o indice se tornar r
-
-	for (int i = start; i <= end; i++)
-	{
-		chosen[index] = i;
-		CombinationRepetitionUtil(chosen, arr, index + 1, r, i, end);
-	}
-	return;
-}
-
-// A função principal que imprime todas as combinações de tamanho r em arr[] de tamanho n 
-// com repetições. Esta função usa principalmente CombinationRepetitionUtil()
-
-void CombinationRepetition(int arr[], int n, int r)
-{
-	// Alocar memória
-	int chosen[r+1];
-
-	// Chamar a função recursiva
-	CombinationRepetitionUtil(chosen, arr, 0, r, 0, n-1);
+int Arranjos(int espacos){
+    /* vetor de cada permutacao. */
+    int *num ;
+    /* quantidade de elementos do vetor. */
+    int n=2 ;
+    /* tamanho de cada permutacao. */
+    int r = espacos;
+    /* controle de loop. */
+    int i, j ;
+    
+    /* Aloca espaco para o vetor num. Lembre-se
+     * que o vetor `num' representa um numero
+     * na base n com r algarismos. */
+    num = (int *)calloc(r+1, sizeof(int)) ;
+    if ( num == NULL ) {
+        printf("erro malloc") ;
+        return -1;
+    }
+ 
+    /* Inicio do algoritmo. */
+    while ( num[r] == 0 ) {
+        for(i=0; i < n; i++) {
+            /* Mostra a permutacao na tela. */
+            for(j=0; j < r; j++) {
+                printf("%s ", vetor[num[j]]) ;
+            }
+            putchar('\n') ;
+ 
+            /* incrementa o algarismo menos
+             * significativo. */
+            num[0]++ ;
+        }
+ 
+        /* distribui os vai-uns. */
+        for(i=0; i < r; i++) {
+            if(num[i] == n) {
+                num[i] = 0;
+                num[i+1]++ ;
+            }
+        }
+    }
+ 
+    free(num) ;
+ 
+    return 0;
 }
